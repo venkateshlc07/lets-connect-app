349bb7e1af55b3940d14d6958b77d60baa5589a5
Venkatesh.Chakravarti <venkatesh.chakravarti@netwitness.com>
2023-06-04 10:50:44 +0530

    initial configurations

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
+# set path to env file
+env_path = Path('.')/ '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Config:
+    """ Set Flask configuration vars from .env file."""
+
+    # Load environment variables
+    TESTING = os.getenv('TESTING')
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SERVER = os.getenv('SERVER')
